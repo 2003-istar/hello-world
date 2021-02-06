f66ca597035e296d1f779aee497c4dc3133d33a6
Istar Abdullahi <53275917+2003-istar@users.noreply.github.com>
2021-02-05 21:48:16 -0500

    Hello world C++
    Commiting hello world program to practice using github.

---
+++ helloword.cpp
@@ -0,0 +1,13 @@
+/*
+This code prints hello world and is to practice using github
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+	
+	return 0;
+}
